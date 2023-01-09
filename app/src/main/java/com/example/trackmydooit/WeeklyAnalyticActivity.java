f0e0aae41c2925f226bc9932d87d5bf96288f2b1
SyazaNabihah <u2000561@siswa.um.edu.my>
2023-01-09 11:54:53 +0800

    history UI

---
+++ app/src/main/java/com/example/trackmydooit/WeeklyAnalyticActivity.java
@@ -0,0 +1,14 @@
+package com.example.trackmydooit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class WeeklyAnalyticActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_weekly_analytic);
+    }
+}
