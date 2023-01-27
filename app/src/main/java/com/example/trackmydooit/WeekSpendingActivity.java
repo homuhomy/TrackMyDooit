b35dbb8709d0d89846340f6f1b3878667afd4ae7
ann3i99 <92704196+ann3i99@users.noreply.github.com>
2023-01-28 03:49:24 +0800

    edit scroll view im monthlyanalytics

--- app/src/main/java/com/example/trackmydooit/WeekSpendingActivity.java
+++ app/src/main/java/com/example/trackmydooit/WeekSpendingActivity.java
@@ -1,163 +1,163 @@
 /*
-package com.example.trackmydooit;
-
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
-import android.os.Bundle;
-import android.view.View;
-import android.widget.ProgressBar;
-import android.widget.TextView;
-
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.Query;
-import com.google.firebase.database.ValueEventListener;
-
-import org.joda.time.DateTime;
-import org.joda.time.Months;
-import org.joda.time.MutableDateTime;
-import org.joda.time.Weeks;
-
-import java.lang.reflect.MalformedParameterizedTypeException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-
-public class WeekSpendingActivity extends AppCompatActivity {
-
-    private Toolbar toolbar;
-    private TextView totalWeekAmount;
-    private ProgressBar progressBar;
-    private RecyclerView recyclerView;
-
-    private WeekSpendingAdapter weekSpendingAdapter;
-    private List<Data> myDataList;
-
-    private FirebaseAuth mAuth;
-    private String onlineUserId = "";
-    private DatabaseReference expensesRef;
-
-    private String type = "";
-
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_week_spending);
-
-        toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        getSupportActionBar().setTitle("Week Spending");
-        totalWeekAmount = findViewById(R.id.totalWeekAmount);
-        progressBar = findViewById(R.id.progressBar);
-        recyclerView = findViewById(R.id.recyclerView);
-
-        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
-        linearLayoutManager.setStackFromEnd(true);
-        linearLayoutManager.setReverseLayout(true);
-        recyclerView.setHasFixedSize(true);
-        recyclerView.setLayoutManager(linearLayoutManager);
-
-        mAuth = FirebaseAuth.getInstance();
-        onlineUserId = mAuth.getCurrentUser().getUid();
-
-        myDataList = new ArrayList<>();
-        weekSpendingAdapter = new WeekSpendingAdapter(WeekSpendingActivity.this, myDataList);
-        recyclerView.setAdapter(weekSpendingAdapter);
-
-        if (getIntent().getExtras() != null) ;
-        type = getIntent().getStringExtra("type");
-        if (type.equals("week")) {
-            //readWeekSpendingItems();
-        } else if (type.equals("month")) {
-            readMonthSpendingItems();
-        }
-    }
-
-    private void readMonthSpendingItems() {
-        MutableDateTime epoch = new MutableDateTime();
-        epoch.setDate(0);
-        DateTime now = new DateTime();
-        Months months = Months.monthsBetween(epoch, now);
-
-        expensesRef = FirebaseDatabase.getInstance().getReference("expenses").child(onlineUserId);
-        Query query = expensesRef.orderByChild("month").equalTo(months.getMonths());
-        query.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
-                myDataList.clear();
-                for(DataSnapshot dataSnapshot : snapshot.getChildren()){
-                    Data data = dataSnapshot.getValue(Data.class);
-                    myDataList.add(data);
-                }
-
-                weekSpendingAdapter.notifyDataSetChanged();
-                progressBar.setVisibility(View.GONE);
-
-                int totalAmount = 0;
-                for(DataSnapshot ds: snapshot.getChildren()){
-                    Map<String, Object> map = (Map<String, Object>)ds.getValue();
-                    Object total = map.get("amount");
-                    int pTotal = Integer.parseInt(String.valueOf(total));
-                    totalAmount += pTotal;
-
-                    totalWeekAmount.setText("Total Month's Spending: RM" + totalAmount);
-                }
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-
-            }
-
-            private void readWeekSpendingItems() {
-
-        MutableDateTime epoch = new MutableDateTime();
-        epoch.setDate(0);
-        DateTime now = new DateTime();
-        Weeks weeks = Weeks.weeksBetween(epoch, now);
-
-        expensesRef = FirebaseDatabase.getInstance().getReference("expenses").child(onlineUserId);
-        Query query = expensesRef.orderByChild("week").equalTo(weeks.getWeeks());
-        query.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
-                myDataList.clear();
-                for(DataSnapshot dataSnapshot : snapshot.getChildren()){
-                    Data data = dataSnapshot.getValue(Data.class);
-                    myDataList.add(data);
-                }
-
-                weekSpendingAdapter.notifyDataSetChanged();
-                progressBar.setVisibility(View.GONE);
-
-                int totalAmount = 0;
-                for(DataSnapshot ds: snapshot.getChildren()){
-                    Map<String, Object> map = (Map<String, Object>)ds.getValue();
-                    Object total = map.get("amount");
-                    int pTotal = Integer.parseInt(String.valueOf(total));
-                    totalAmount += pTotal;
-
-                    totalWeekAmount.setText("Total Week's Spending: RM" + totalAmount);
-                }
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-
-            }
-        });
-
-    }
-});
-    }
-}
-
+//package com.example.trackmydooit;
+//
+//import androidx.annotation.NonNull;
+//import androidx.appcompat.app.AppCompatActivity;
+//import androidx.appcompat.widget.Toolbar;
+//import androidx.recyclerview.widget.LinearLayoutManager;
+//import androidx.recyclerview.widget.RecyclerView;
+//
+//import android.os.Bundle;
+//import android.view.View;
+//import android.widget.ProgressBar;
+//import android.widget.TextView;
+//
+//import com.google.firebase.auth.FirebaseAuth;
+//import com.google.firebase.database.DataSnapshot;
+//import com.google.firebase.database.DatabaseError;
+//import com.google.firebase.database.DatabaseReference;
+//import com.google.firebase.database.FirebaseDatabase;
+//import com.google.firebase.database.Query;
+//import com.google.firebase.database.ValueEventListener;
+//
+//import org.joda.time.DateTime;
+//import org.joda.time.Months;
+//import org.joda.time.MutableDateTime;
+//import org.joda.time.Weeks;
+//
+//import java.lang.reflect.MalformedParameterizedTypeException;
+//import java.util.ArrayList;
+//import java.util.List;
+//import java.util.Map;
+//
+//public class WeekSpendingActivity extends AppCompatActivity {
+//
+//    private Toolbar toolbar;
+//    private TextView totalWeekAmount;
+//    private ProgressBar progressBar;
+//    private RecyclerView recyclerView;
+//
+//    private WeekSpendingAdapter weekSpendingAdapter;
+//    private List<Data> myDataList;
+//
+//    private FirebaseAuth mAuth;
+//    private String onlineUserId = "";
+//    private DatabaseReference expensesRef;
+//
+//    private String type = "";
+//
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_week_spending);
+//
+//        toolbar = findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
+//        getSupportActionBar().setTitle("Week Spending");
+//        totalWeekAmount = findViewById(R.id.totalWeekAmount);
+//        progressBar = findViewById(R.id.progressBar);
+//        recyclerView = findViewById(R.id.recyclerView);
+//
+//        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+//        linearLayoutManager.setStackFromEnd(true);
+//        linearLayoutManager.setReverseLayout(true);
+//        recyclerView.setHasFixedSize(true);
+//        recyclerView.setLayoutManager(linearLayoutManager);
+//
+//        mAuth = FirebaseAuth.getInstance();
+//        onlineUserId = mAuth.getCurrentUser().getUid();
+//
+//        myDataList = new ArrayList<>();
+//        weekSpendingAdapter = new WeekSpendingAdapter(WeekSpendingActivity.this, myDataList);
+//        recyclerView.setAdapter(weekSpendingAdapter);
+//
+//        if (getIntent().getExtras() != null) ;
+//        type = getIntent().getStringExtra("type");
+//        if (type.equals("week")) {
+//            //readWeekSpendingItems();
+//        } else if (type.equals("month")) {
+//            readMonthSpendingItems();
+//        }
+//    }
+//
+//    private void readMonthSpendingItems() {
+//        MutableDateTime epoch = new MutableDateTime();
+//        epoch.setDate(0);
+//        DateTime now = new DateTime();
+//        Months months = Months.monthsBetween(epoch, now);
+//
+//        expensesRef = FirebaseDatabase.getInstance().getReference("expenses").child(onlineUserId);
+//        Query query = expensesRef.orderByChild("month").equalTo(months.getMonths());
+//        query.addValueEventListener(new ValueEventListener() {
+//            @Override
+//            public void onDataChange(@NonNull DataSnapshot snapshot) {
+//                myDataList.clear();
+//                for(DataSnapshot dataSnapshot : snapshot.getChildren()){
+//                    Data data = dataSnapshot.getValue(Data.class);
+//                    myDataList.add(data);
+//                }
+//
+//                weekSpendingAdapter.notifyDataSetChanged();
+//                progressBar.setVisibility(View.GONE);
+//
+//                int totalAmount = 0;
+//                for(DataSnapshot ds: snapshot.getChildren()){
+//                    Map<String, Object> map = (Map<String, Object>)ds.getValue();
+//                    Object total = map.get("amount");
+//                    int pTotal = Integer.parseInt(String.valueOf(total));
+//                    totalAmount += pTotal;
+//
+//                    totalWeekAmount.setText("Total Month's Spending: RM" + totalAmount);
+//                }
+//            }
+//
+//            @Override
+//            public void onCancelled(@NonNull DatabaseError error) {
+//
+//            }
+//
+//            private void readWeekSpendingItems() {
+//
+//        MutableDateTime epoch = new MutableDateTime();
+//        epoch.setDate(0);
+//        DateTime now = new DateTime();
+//        Weeks weeks = Weeks.weeksBetween(epoch, now);
+//
+//        expensesRef = FirebaseDatabase.getInstance().getReference("expenses").child(onlineUserId);
+//        Query query = expensesRef.orderByChild("week").equalTo(weeks.getWeeks());
+//        query.addValueEventListener(new ValueEventListener() {
+//            @Override
+//            public void onDataChange(@NonNull DataSnapshot snapshot) {
+//                myDataList.clear();
+//                for(DataSnapshot dataSnapshot : snapshot.getChildren()){
+//                    Data data = dataSnapshot.getValue(Data.class);
+//                    myDataList.add(data);
+//                }
+//
+//                weekSpendingAdapter.notifyDataSetChanged();
+//                progressBar.setVisibility(View.GONE);
+//
+//                int totalAmount = 0;
+//                for(DataSnapshot ds: snapshot.getChildren()){
+//                    Map<String, Object> map = (Map<String, Object>)ds.getValue();
+//                    Object total = map.get("amount");
+//                    int pTotal = Integer.parseInt(String.valueOf(total));
+//                    totalAmount += pTotal;
+//
+//                    totalWeekAmount.setText("Total Week's Spending: RM" + totalAmount);
+//                }
+//            }
+//
+//            @Override
+//            public void onCancelled(@NonNull DatabaseError error) {
+//
+//            }
+//        });
+//
+//    }
+//});
+//    }
+//}
+//
 */
